9afb7c51611d1bbbe0e6e0c99e31b47940ea8c29
Robin Jones <RobinJones335@gmail.com>
2020-03-31 16:02:40 -0400

    moved files to the correct repo

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+
+router.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname + 'index.html'), (err)=>{
+      if(err) throw err;
+  });
+})
+
+
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
